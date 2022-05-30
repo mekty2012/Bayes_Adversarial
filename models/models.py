525eadb5e33cf774116c07ddbccb22a278a5eb2d
TaeYoung Kim <taeyoungkim21@kaist.ac.kr>
2022-05-31 03:31:09 +0900

    Implement ensemble

--- models/models.py
+++ models/models.py
@@ -1,3 +1,5 @@
+# Based on https://d2l.ai/chapter_convolutional-modern/resnet.html
+
 import numpy as np
 import torch
 from torch import nn
@@ -116,3 +118,61 @@ def dropout_resnet18(dropout_rate, dropout_type, init_dict):
                        nn.Flatten(),
                        layers.Dropout_Linear(512, 10, dropout_rate=dropout_rate, dropout_type=dropout_type, init_dict=init_dict))
 
+class Residual(nn.Module):
+  def __init__(self, input_channels, num_channels,
+               use_1x1conv=False, strides=1):
+    super().__init__()
+    self.conv1 = nn.Conv2d(input_channels, num_channels,
+                           kernel_size=3, padding=1, stride=strides)
+    self.conv2 = nn.Conv2d(num_channels, num_channels,
+                           kernel_size=3, padding=1)
+    if use_1x1conv:
+      self.conv3 = nn.Conv2d(input_channels, num_channels,
+                               kernel_size=1, stride=strides)
+    else:
+      self.conv3 = None
+    self.bn1 = nn.BatchNorm2d(num_channels)
+    self.bn2 = nn.BatchNorm2d(num_channels)
+
+  def forward(self, X):
+    Y = nn.functional.relu(self.bn1(self.conv1(X)))
+    Y = self.bn2(self.conv2(Y))
+    if self.conv3:
+      X = self.conv3(X)
+    Y += X
+    return nn.functional.relu(Y)
+
+def resnet_block(input_channels, num_channels, num_residuals,
+                 first_block=False):
+  blk = []
+  for i in range(num_residuals):
+    if i == 0 and not first_block:
+      blk.append(Residual(input_channels, num_channels,
+                          use_1x1conv=True, strides=2))
+    else:
+      blk.append(Residual(num_channels, num_channels))
+  return blk
+
+class ensemble_resnet18(nn.Module):
+
+  def __init__(self, num_ensemble):
+    super().__init__()
+    self.models = nn.ModuleList()
+    for _ in range(num_ensemble):
+      b1 = nn.Sequential(nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+                         nn.BatchNorm2d(64), nn.ReLU(),
+                         nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+      b2 = nn.Sequential(*resnet_block(64, 64, 2, first_block=True))
+      b3 = nn.Sequential(*resnet_block(64, 128, 2))
+      b4 = nn.Sequential(*resnet_block(128, 256, 2))
+      b5 = nn.Sequential(*resnet_block(256, 512, 2))
+      net = nn.Sequential(b1, b2, b3, b4, b5,
+                          nn.AdaptiveAvgPool2d((1,1)),
+                          nn.Flatten(), nn.Linear(512, 10))
+      self.models.append(net)
+  
+  def forward(self, x):
+    res = []
+    for model in self.models:
+      res.append(model(x))
+    return res
